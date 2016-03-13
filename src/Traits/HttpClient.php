0128cceba2b94a10abc1d67f83c25f1d503bdc81
Jonathan Webb <webbj74@users.noreply.github.com>
2016-03-13 13:56:17 +0000

    Removes dependency on Guzzle.
    It appears that there is no code which actually uses Guzzle since @3ffc3f8e.
    
    This commit removes three public methods from the HttpClient trait:
    - setHttpClient
    - getHttpClient
    - getJson
    
    Presently this trait is only used in this project in the Manifest and Github
    classes.

--- src/Traits/HttpClient.php
+++ src/Traits/HttpClient.php
@@ -2,52 +2,10 @@
 
 namespace Cpliakas\ManifestPublisher\Traits;
 
-use Guzzle\Http\Client;
 use Symfony\Component\Filesystem\Filesystem;
 
 trait HttpClient
 {
-    /**
-     * @var Client
-     */
-    private $httpClient;
-
-    /**
-     * @param  Client $client
-     *
-     * @return Repository
-     */
-    public function setHttpClient(Client $client)
-    {
-        $this->httpClient = $client;
-        return $this;
-    }
-
-    /**
-     * @return Client
-     */
-    public function getHttpClient()
-    {
-        if (!isset($this->httpClient)) {
-            $this->httpClient = new Client();
-        }
-        return $this->httpClient;
-    }
-
-    /**
-     * @param  string $url
-     *
-     * @return array
-     */
-    public function getJson($url)
-    {
-        return $this->getHttpClient()
-            ->get($url)
-            ->send()
-            ->json()
-        ;
-    }
-
     /**
      * @param string  $url
      * @param string  $filename
